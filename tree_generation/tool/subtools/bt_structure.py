2a3fe573e628ef1ac5090ae90759206119775d89
ipa-rwu <ruichao.wu@ipa.fraunhofer.de>
2020-09-03 13:58:07 +0200

    init

---
+++ tree_generation/tool/subtools/bt_structure.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+import xml.etree.ElementTree as ET
+
+class CreateBTStructure():
+    def __init__:
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
+    def task_structure(self):
+        
