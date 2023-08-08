4690fb3ee8820df50506d76a53517621ac17e2ed
Ruichao Wu <ruichao.wu@ipa.fraunhofer.de>
2023-08-09 00:42:10 +0200

    fix licneses and reformat launch file in black

--- detect_aruco_marker_skill/launch/start_detect_aruco_marker_action_server.launch.py
+++ detect_aruco_marker_skill/launch/start_detect_aruco_marker_action_server.launch.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http:#www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -33,9 +33,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration("use_sim_time")
     params_file = LaunchConfiguration("params_file")
 
-    stdout_linebuf_envvar = SetEnvironmentVariable(
-        "RCUTILS_LOGGING_BUFFERED_STREAM", "1"
-    )
+    stdout_linebuf_envvar = SetEnvironmentVariable("RCUTILS_LOGGING_BUFFERED_STREAM", "1")
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         "use_sim_time",
